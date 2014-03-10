727cc2642fdce5261e4372eb718dddc7a5e67ecd
arnelgt <prime.arnel@gmail.com>
2014-03-10 15:51:23 +0800

    removed unused files from previous commit
    color picker and stuff

--- assets/js/row-with-trash.js
+++
@@ -1,5 +0,0 @@
-$('.row-with-trash').hover(function() {
-	$(this).find('.row-trash-icon').removeClass('absolute-hide');
-}, function() {
-	$(this).find('.row-trash-icon').addClass('absolute-hide');
-});

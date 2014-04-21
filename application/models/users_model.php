1080ecff2b564d74d7f58cd7b707c4d3761336e7
Jolo Ortiz <jolo.ortiz@gmail.com>
2014-04-21 22:31:31 +0800

    Users CRUD fixes and Edit Module

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -19,14 +19,34 @@ class Users_model extends CI_Model{
 		return $query->result_array();
    }
 
-	function get_by_username($username) {
+   	function get_by_username($username) {
 		$this->db->where('u_username', $username);
+		$query = $this->db->get('users');
+		return $query->result_array();
+	}
+
+	function get_user_by_id($id) {
+		$this->db->where('u_id', $id);
 		$query = $this->db->get('users');
 		return $query->result_array();
 	}
 
 	function is_username_existing($username){
 		$this->db->where('u_username', $username);
+		$query = $this->db->get('users');
+		$total = $query->num_rows();
+
+		if($total > 0){
+			return TRUE;
+		}
+		else{
+			return FALSE;
+		}
+	}
+
+	function is_username_existing_edit($username, $id){
+		$this->db->where('u_username', $username);
+		$this->db->where('u_id !=', $id);
 		$query = $this->db->get('users');
 		$total = $query->num_rows();
 

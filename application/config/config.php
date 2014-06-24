3a378926877e0a4b45f3a90734e4963cac6ebef6
arnelgt <prime.arnel@gmail.com>
2014-06-24 14:35:30 +0800

    localhost base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://192.168.3.117/cyms';
+$config['base_url']	= 'http://localhost/cyms';
 
 /*
 |--------------------------------------------------------------------------
